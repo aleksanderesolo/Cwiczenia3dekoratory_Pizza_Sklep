c2638ef25c48a5b3d62b7b46b3b2d38da548b725
aleksanderesolo <163632389+aleksanderesolo@users.noreply.github.com>
2024-05-22 23:00:35 +0200

    Delete ćwiczenia 3 (dekoratory)/Cwiczenia4DekoratoryPizza directory

--- ćwiczenia 3 (dekoratory)/Cwiczenia4DekoratoryPizza/Pizza.java
+++
@@ -1,9 +0,0 @@
-package Cwiczenia4DekoratoryPizza;
-
-public abstract class Pizza {
-    String description = "Unknown Pizza";
-    public String getDescription() {
-        return description;
-    }
-    public abstract double cost();
-}
--- ćwiczenia 3 (dekoratory)/Cwiczenia4DekoratoryPizza/Topping.java
+++
@@ -1,5 +0,0 @@
-package Cwiczenia4DekoratoryPizza;
-
-public abstract class Topping extends Pizza {
-    public abstract String getDescription();
-}
